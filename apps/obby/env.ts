9f46eb7042efc2eb1c22aacbf9f29e4ff9eacdd7
Sree Narayanan <sreeaadhi07@gmail.com>
2025-09-03 03:02:58 +0400

    Update env.ts

--- apps/obby/env.ts
+++ apps/obby/env.ts
@@ -1,11 +1,11 @@
 import { keys as ai } from '@repo/ai/keys';
 import { keys as database } from '@repo/database/keys';
-import { keys as email } from '@repo/email/keys';
 import { keys as core } from '@repo/next-config/keys';
+import { keys as storage } from '@repo/storage/keys';
 import { createEnv } from '@t3-oss/env-nextjs';
 
 export const env = createEnv({
-  extends: [ai(), core(), database(), email()],
+  extends: [ai(), core(), database(), storage()],
   server: {},
   client: {},
   runtimeEnv: {},
